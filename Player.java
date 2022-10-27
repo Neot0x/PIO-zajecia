44bdeae5d5ba506deb49a587e87a8e6c575b9627
Neot0x <57873348+Neot0x@users.noreply.github.com>
2022-10-27 11:15:05 +0200

    kolejne zajęcia

--- Player.java
+++ Player.java
@@ -20,3 +20,40 @@ public class Player {
         }
         
 }
+/////
+package com.mycompany.zajecia;
+
+import java.util.Random;
+
+public class Player{
+        
+        private Random brain = new Random();
+        private String name = "Player"; // dobrze jest przypisać poprawną wartość i blokujemy niepoprawne wartości
+        
+        public Player(){}
+        public Player(String name){
+            setName(name);
+        }
+        
+        public void setName(String name){
+            //if(name != null && !name.isEmpty()) //w tej kolejności metoda nie będzie wywoływana dla null, bo
+            if(name != null && !name.isEmpty())
+                this.name = name;
+            else
+                System.err.println("Bledna nazwa");
+        }
+        
+        public String getName(){
+            return name;
+        }
+        
+        public int guess(){
+            return brain.nextInt(6)+1;
+        }
+        
+}
+
+/*  REGEX - wyrażenia regularne
+    regex101.com
+    ^[a-zA-Z0-9.\-_+]{3,}$
+*/
