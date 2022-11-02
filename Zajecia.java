4c2aaf28bce01bb824d13562c099543d19f53c3c
Neot0x <57873348+Neot0x@users.noreply.github.com>
2022-11-02 22:52:03 +0100

    Zajecia 27.10.2022

--- Zajecia.java
+++ Zajecia.java
@@ -7,40 +7,6 @@ public class Zajecia {
         
         Random dice = new Random();
         Player player = new Player();
-        player.setName("Player");
-        
-        int number;
-        int guess;
-        do{
-        
-        number = dice.nextInt(6)+1;
-        System.out.println(number);
-        
-        guess = player.guess();
-        System.out.println(player.getName() + ": " + guess );
-        
-        if(guess == number){
-            System.out.println("Zgadles");
-        }else{
-            System.out.println("Nie zgadles");
-        }
-        }while(guess != number);
-    }
-}
-/*zasady otwarte zamknięte
-Regresja -
-Refaktoryzacja +
-*/
-
-package com.mycompany.zajecia;
-
-import java.util.Random;
-
-public class Zajecia {
-    public static void main(String[] args) {
-        
-        Random dice = new Random();
-        Player player = new Player();
         //player.setName(null);
         
         int number;
@@ -63,6 +29,10 @@ public class Zajecia {
 }
 
 /*
+zasady otwarte zamknięte
+Regresja -
+Refaktoryzacja +
+
 Przeciążanie operatorów - można nadać operatorom inne znaczenie
 
 
@@ -77,4 +47,8 @@ else
 nigdy nie stosować "==" do Stringów
 t.euquals() - to stosować
 częste pytanie na inżynierce: czym są i co robią konstruktory
+
+REGEX - wyrażenia regularne
+    regex101.com
+    ^[a-zA-Z0-9.\-_+]{3,}$
 */
