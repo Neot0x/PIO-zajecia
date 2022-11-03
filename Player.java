7afe006727e55fb96ad6fed10f524d9d36f57b13
Neot0x <57873348+Neot0x@users.noreply.github.com>
2022-11-03 11:15:06 +0100

    Zajęcia 03.11.2022

--- Player.java
+++ Player.java
@@ -33,3 +33,40 @@ public class Player{
     regex101.com
     ^[a-zA-Z0-9.\-_+]{3,}$
 */
+
+package com.mycompany.zajecia;
+
+import java.util.Random;
+
+public class Player{
+        
+        private Random brain = new Random();
+        private String name = "Player";
+        
+        public Player(){}
+        public Player(String name){
+            setName(name);
+        }
+        
+        public void setName(String name){
+            if(name != null && name.matches("^[a-zA-Z\\d._\\-]{3,}$"))
+                this.name = name;
+            else
+//              System.err.println("Bledna nazwa gracza");
+                throw new IllegalArgumentException("Niepoprawne imie");
+        }
+        
+        public String getName(){
+            return name;
+        }
+        
+        public int guess(){
+            return brain.nextInt(6)+1;
+        }
+        
+}
+
+/*  REGEX - wyrażenia regularne
+    regex101.com
+    ^[a-zA-Z0-9.\-_+]{3,}$
+*/
