613ac2f0afe6f06ef776e4cbe9eeab985e9fb13c
Neot0x <57873348+Neot0x@users.noreply.github.com>
2022-11-03 11:14:40 +0100

    Zajęcia 03.11.2022

--- Zajecia.java
+++ Zajecia.java
@@ -52,3 +52,57 @@ REGEX - wyrażenia regularne
     regex101.com
     ^[a-zA-Z0-9.\-_+]{3,}$
 */
+
+package com.mycompany.zajecia;
+
+import java.util.Random;
+
+public class Zajecia {
+    public static void main(String[] args) {
+        
+        Random dice = new Random();
+        
+        Player player;
+        
+        if(mode)
+            player = new PlayerHuman();
+        else
+            player = new Player();
+        //player.setName("abc");
+        /*
+        try{
+            player.setName("ab");
+        }
+        catch(IllegalArgumentException ex){
+            System.err.println("Blad " + ex.getMessage());
+        }
+        catch(NullPointerException ex){
+            System.err.println("NULL!");
+        }
+        */
+        
+        int number;
+        int guess;
+        do{
+        
+        number = dice.nextInt(6)+1;
+        System.out.println(number);
+        
+        guess = player.guess();
+        System.out.println(player.getName() + ": " + guess );
+        
+        if(guess == number){
+            System.out.println("Zgadles");
+        }else{
+            System.out.println("Nie zgadles");
+        }
+        }while(guess != number);
+    }
+}
+
+/*
+System.out.println("Cytat: \"ble ble\""); - \ znak ucieczki
+rzeczy w try tyle żeby wszystko było na ekranie
+nie zagnieżdżać try w try
+przesłanianie/nadpisywanie Overdrive - zasłania to samo co było w klasie "bazowej"
+*/
